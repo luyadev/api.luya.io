e30d2884e5d205326e024fb86ce51282cfbe6d74
Basil Suter <git@nadar.io>
2022-11-17 13:23:04 +0100

    luya boot as default

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf dist/
 
 LUYA=vendor/luyadev/luya-core
 ADMIN=vendor/luyadev/luya-module-admin/src
@@ -9,4 +10,7 @@ YII=vendor/yiisoft/yii2
 HELPERS=vendor/luyadev/yii-helpers/src
 OUTPUT=dist
 
-./vendor/bin/apidoc api $LUYA,$ADMIN,$CMS,$YII,$SUITE,$HELPERS $OUTPUT --interactive=0
+./vendor/bin/apidoc api $LUYA,$ADMIN,$CMS,$YII,$SUITE,$HELPERS $OUTPUT --interactive=0 --exclude="./vendor/yiisoft" --page-title="LUYA"
+
+rm -rf dist/index.html
+cp dist/luya-boot.html dist/index.html
